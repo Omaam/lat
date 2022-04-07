30e0aa78efdf78084c4a127aa650b4ee46472fdf
Omaam <omama.tomoki@gmail.com>
2022-04-07 17:36:16 +0900

    Add

---
+++ lat/README.md
@@ -0,0 +1,30 @@
+# LAT modules
+
+## basic.py
+
+## ccf.py
+Ailias for correlation. This will be removed in future version.
+
+## correlation.py
+
+## fits.py
+
+## io.py
+Modules to handle astro data like fits, qdp, and so on.
+
+## j1820
+
+## lchandler.py
+
+## lcsimulation.py
+Light curve simulation.
+No longer suppport this.
+Use curvesim package.
+
+## lightcurve.py
+
+## preprocess.py
+
+## time.py
+
+## util.py
---
+++ lat/preprocess.py
@@ -0,0 +1,31 @@
+"""Useful functions when preprocessing.
+"""
+import numpy as np
+
+
+def lcsplit(lcdata, dt, min_points=1, min_gap=None):
+    """
+    """
+
+    if min_gap is None:
+        min_gap = dt
+
+    # search for irregular interval
+    diff = np.round(np.diff(lcdata[:, 0]), 6)
+    # idxs_split = np.array(np.where(diff != dt))
+    idxs_split = np.array(np.where(diff > min_gap)) + 1
+    # print(f'Number of segment: {idxs_split.shape[1]}')
+
+    # add 0 and -1 to idxs_split
+    idxs_split = np.insert(idxs_split, 0, 0)
+    idxs_split = np.append(idxs_split, len(lcdata)-1)
+
+    # generate split lightcurves
+    for k in range(len(idxs_split)-1):
+        lcdata_splited = lcdata[idxs_split[k]:idxs_split[k+1]]
+        if min_points <= len(lcdata_splited):
+            yield lcdata[idxs_split[k]:idxs_split[k+1]]
+
+
+def moving_average(a, n_ave):
+    return np.convolve(a, np.ones(n_ave), 'valid') / n_ave
