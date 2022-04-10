e8f0a64e267d00bedd45d0205c1289ef82323e66
Omaam <omama.tomoki@gmail.com>
2022-04-10 13:20:18 +0900

    Add new func

--- lat/util.py
+++ lat/util.py
@@ -3,12 +3,20 @@
 import numpy as np
 
 
+def calc_energy_position(energy_edges, position_rate=0.5):
+    """Calculate energies at which position_rate corresponds to."""
+    out = []
+    for i in range(len(energy_edges)-1):
+        width = energy_edges[i+1] - energy_edges[i]
+        out.append(energy_edges[i] + position_rate*width)
+    return out
+
+
 def calc_energy_centers_and_errors(energy_edges):
     """Calculate energy centers and errors."""
     energy_edges = np.array(energy_edges)
 
     energy_centers = (energy_edges[1:] + energy_edges[:-1]) / 2
-
     energy_errors_upper = energy_edges[1:] - energy_centers
     energy_errors_lower = energy_centers - energy_edges[:-1]
     energy_errors = np.vstack(
