4a5b6eace120f9488623645a368641ce155d06e1
Omaam <omama.tomoki@gmail.com>
2022-08-18 18:30:25 +0900

    Add nicertime

--- lat/convert.py
+++ lat/convert.py
@@ -6,3 +6,11 @@ def convert_reftime2mjd(time):
     # reference:
     # https://heasarc.gsfc.nasa.gov/docs/nicer/analysis_threads/time/
     return 56658 + time/3600/24
+
+
+def convert_nicertime2mjd(time):
+    """Convert nicertime to MJD.
+
+    Alias for 'convert_reftime2mjd'
+    """
+    return convert_reftime2mjd(time)
